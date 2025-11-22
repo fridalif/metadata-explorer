23131ae6b2bbb6fa63de1899e4b8fb244f2460f6
fridalif <fridalif2@gmail.com>
2025-11-22 15:55:22 +0300

    парсинг флагов

--- src/png/png.rs
+++ src/png/png.rs
@@ -27,25 +27,62 @@ impl PngParser {
         }
     }
     pub fn parse(&mut self, flags: &Vec<String>) {
-        for flag in flags {
-            if flag == "--help" {
+        let vec_len = flags.len();
+        for i in 0..vec_len {
+            if flags[i] == "--help" {
                 self.mode = WorkMode::Help;
-                continue;
+                return;
             }
-            if flag == "--read" && self.mode == WorkMode::Help {
+            if flags[i] == "--read" && self.mode == WorkMode::Help {
                 self.mode = WorkMode::Read;
                 continue;
             }
-            if flag == "--write" && self.mode == WorkMode::Help {
+            if flags[i] == "--write" && self.mode == WorkMode::Help {
                 self.mode = WorkMode::Write;
                 continue;
             }
-            if flag == "--update" && self.mode == WorkMode::Help {
+            if flags[i] == "--update" && self.mode == WorkMode::Help {
                 self.mode = WorkMode::Update;
                 continue;
             }
-            if flag == "--delete" && self.mode == WorkMode::Help {
+            if flags[i] == "--delete" && self.mode == WorkMode::Help {
                 self.mode = WorkMode::Delete;
+                continue;
+            }
+            if flags[i] == "--filename" {
+                if i + 1 > vec_len {
+                    self.mode = WorkMode::Help;
+                    println!("No filename provided");
+                    return;
+                }
+                self.file.push(flags[i + 1].clone());
+                continue;
+            }
+            if flags[i] == "--chunk_type" {
+                if i + 1 > vec_len {
+                    self.mode = WorkMode::Help;
+                    println!("No chunk type provided");
+                    return;
+                }
+                self.chunk_type = flags[i + 1].clone();
+                continue;
+            }
+            if flags[i] == "--position" {
+                if i + 1 > vec_len {
+                    self.mode = WorkMode::Help;
+                    println!("No position provided");
+                    return;
+                }
+                self.position = flags[i + 1].parse().unwrap();
+                continue;
+            }
+            if flags[i] == "--data" {
+                if i + 1 > vec_len {
+                    self.mode = WorkMode::Help;
+                    println!("No data provided");
+                    return;
+                }
+                self.data = flags[i + 1].clone().into_bytes();
                 continue;
             }
         }
